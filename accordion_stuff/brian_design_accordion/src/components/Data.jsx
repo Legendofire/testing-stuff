89bf700088126d294a741c01ca94aae39469395b
rdionis-elshoff <dionisio@elsweyer-hoffmann.de>
2021-08-17 16:53:14 +0200

    updating testing-stuff

---
+++ accordion_stuff/brian_design_accordion/src/components/Data.jsx
@@ -0,0 +1,8 @@
+export const Data = [
+
+    {question: "What do you call a dog magician?", answer: "A Labracadrabra-dor."},
+
+    {question: "What do you call a funny mountain?", answer: "Hill-arious."},
+    
+    {question: "What did the astronaud said when he crashed into the moon?", answer: "I Apollo-gize."}
+]
