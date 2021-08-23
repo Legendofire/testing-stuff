86a789e65a78becb7d0259745fca51f2548f8fe0
rdionis-elshoff <dionisio@elsweyer-hoffmann.de>
2021-08-23 15:45:31 +0200

    adding draggable cards

--- drag_and_drop_stuff/tyler_potts/src/App.js
+++ drag_and_drop_stuff/tyler_potts/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import Board from './components/Board';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-     <h1>Drag and Drop Raquel</h1>
+      <main className="flexbox">
+        <Board id="board-1" className="board">
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card One</p>
+          </Card>
+        </Board>
+        <Board id="board-1" className="board">
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card Two</p>
+          </Card>
+        </Board>
+      </main>
     </div>
   );
 }
