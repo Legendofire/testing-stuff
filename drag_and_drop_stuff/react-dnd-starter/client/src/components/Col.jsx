18f096d22aefb6d48bb0904e03ebef699ad3c3c8
rdionis-elshoff <dionisio@elsweyer-hoffmann.de>
2021-08-24 15:58:44 +0200

    updating react-dnd-starter (too complicateed)

--- drag_and_drop_stuff/react-dnd-starter/client/src/components/Col.jsx
+++ drag_and_drop_stuff/react-dnd-starter/client/src/components/Col.jsx
@@ -1,5 +1,8 @@
+import React from 'react'
+
 const Col = ({isOver, children}) => {
     const className = isOver ? "highlight-region" : ""
+    // it will highlight the column when an item is being dragged over it
 
     return (
         <div className={`col${className}`}>
@@ -9,3 +12,4 @@ const Col = ({isOver, children}) => {
 }
 
 export default Col
+// this is gonna know whether an item is being dragged over the column or not
--- drag_and_drop_stuff/react-dnd-starter/client/src/types.js
+++ drag_and_drop_stuff/react-dnd-starter/client/src/types.js
@@ -1,3 +1,3 @@
-const ITEM_TYPE = "ITEM" // react dnd requires that you specific the type of what it is that you want to drag
+const ITEM_TYPE = "ITEM" // react dnd requires that you specify the type of what it is that you want to drag
 
 export default ITEM_TYPE
