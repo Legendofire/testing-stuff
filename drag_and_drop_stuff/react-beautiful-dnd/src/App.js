c7ab1f9fd63db81872f1372392a96f116974c63d
rdionis-elshoff <dionisio@elsweyer-hoffmann.de>
2021-08-26 16:06:57 +0200

    adding projects to testing-stuff

---
+++ drag_and_drop_stuff/react-beautiful-dnd/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import initialData from './initial-data'
+
+const App = () => {
+const [initialData, setInitialData] = useState(initialData)
+
+return(
+
+    setState = () => columnOrder.map(columnId =>{
+
+    })
+)
+}
+
+export default App
