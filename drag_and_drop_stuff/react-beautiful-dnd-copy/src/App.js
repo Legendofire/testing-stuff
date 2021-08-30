4be8a9ad9676ed1eabc035697b3fcaad8c42b906
rdionis-elshoff <dionisio@elsweyer-hoffmann.de>
2021-08-30 17:07:24 +0200

    created copy of react-beautiful-dnd for experiment

---
+++ drag_and_drop_stuff/react-beautiful-dnd-copy/src/App.js
@@ -0,0 +1,120 @@
+import React, {useState} from "react"
+import "@atlaskit/css-reset"
+import styled from 'styled-components'
+import { DragDropContext } from 'react-beautiful-dnd'
+import initialData from "./initial-data"
+import Column from "./Column"
+
+const Container = styled.div`
+    display: flex;
+`
+
+const App = () => {
+   
+    const [state, setState] = useState(initialData)
+    
+    const onDragEnd = result => {
+    // it is the responsibility of your onDrag function to synchronously update your state to reflect the Drag and Drop result
+        const  {destination, source, draggableId} = result
+
+        if(!destination){
+        return
+        }
+
+        // if (
+        //     destination.droppableId === source.droppableId && // when we are dragging and dropping stuff inside the same element
+        //     destination.index === source.index
+        // ) {
+        //     return
+        // }
+        // const column = state.columns[source.droppableId]
+        const start = state.columns[source.droppableId]
+        const finish = state.columns[destination.droppableId]
+
+        if (start === finish) {
+
+            const newTaskIds = Array.from(start.taskIds)
+            newTaskIds.splice(source.index, 1) // this means: from the index "source.index", we want to remove 1 item
+            newTaskIds.splice(destination.index, 0, draggableId) // this means: from the index "destination.index" I am going to remove nothing (0) and I am going to add the draggableId
+        
+            const newColumn = {
+                ...start,
+                taskIds: newTaskIds,
+            }
+
+            setState({          
+                ...state,
+                columns:{
+                  ...state.columns,
+                [newColumn.id]: newColumn, 
+                }
+            })}
+        // Moving from one list to another
+        const startTaskIds = Array.from(start.taskIds)
+        startTaskIds.splice(source.index, 1)
+        const newStart = {
+            ...start,
+            taskIds: startTaskIds,
+        }
+
+        const finishTaskIds = Array.from(finish.taskIds)
+        finishTaskIds.splice(destination.index, 0, draggableId)
+        const newFinish = {
+            ...finish,
+            taskIds: finishTaskIds,
+        }
+
+        setState({
+            ...state,
+            columns: {
+                ...state.columns,
+                [newStart.id]: newStart, 
+                [newFinish.id]: newFinish,
+            }
+
+        })
+      }
+
+    
+
+    return (
+    <DragDropContext
+        //onDragStart
+        //onDragUpdate // called when something changes during a drag
+        onDragEnd={onDragEnd}
+        // called at the end of a drag (this is the ONLY REQUIRED CALLBACK for the DragDropContext)
+    >
+        <Container>
+
+
+    {state.columnOrder.map(columnId => {
+        const column = state.columns[columnId]
+        const tasks = column.taskIds.map(taskId => state.tasks[taskId])
+        
+        return (
+            <Column key={column.id} column={column} tasks={tasks}/>
+            )
+        })}
+        </Container>
+    </DragDropContext>
+    )
+}
+
+export default App
+
+// class App extends React.Component{
+
+//     state = initialData;
+
+//     render(){
+
+//         return this.state.columnOrder.map(columnId => {
+//             const column = this.state.columns[columnId]
+//             const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
+
+//             return <Column key={column.id} column={column} tasks={tasks}/>
+//         })
+//     }        
+// }
+
+// export default App
---
+++ drag_and_drop_stuff/react-beautiful-dnd-copy/src/initial-data.js
@@ -0,0 +1,29 @@
+const initialData = {
+    tasks: {
+        "task-1": { id: "task-1", content: "Arrumar a entrada" },
+        "task-2": { id: "task-2", content: "Arrumar sofá" },
+        "task-3": { id: "task-3", content: "Descongelar fígado" },
+        "task-4": { id: "task-4", content: "Fazer jantar" }
+    },
+    columns: {
+        "column-1": {
+            id: "column-1",
+            title: "To do",
+            taskIds:["task-1", "task-2", "task-3", "task-4",]
+        },
+        "column-2": {
+            id: "column-2",
+            title: "In Progress",
+            taskIds:[]
+            },
+        "column-3": {
+            id: "column-3",
+            title: "Done",
+            taskIds:[]
+                }
+    },
+    // Facilitate reordering of the columns
+    columnOrder: ["column-1", "column-2", "column-3"] // this is the property that will be used to record the order of the columns
+}
+
+export default initialData
