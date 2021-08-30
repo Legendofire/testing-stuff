e7fee199e21ae2347fe1da7f4055ca04187d1887
rdionis-elshoff <dionisio@elsweyer-hoffmann.de>
2021-08-30 16:47:44 +0200

    created a copy of react-beaut-dnd to experiment

---
+++ drag_and_drop_stuff/react-beautiful-dnd (copy)/src/Column.jsx
@@ -0,0 +1,67 @@
+ import React from 'react'
+ import styled from "styled-components"
+ import {Droppable} from "react-beautiful-dnd"
+ import Task from "./Task"
+
+
+const Container = styled.div`
+    margin: 8px;
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+    width: 220px;
+    display: flex;
+    flex-direction: column;`
+
+const Title = styled.h3`
+    color: purple;
+    padding: 8px`
+
+const TaskList = styled.div`
+    div:hover {cursor: pointer};
+    padding: 8px;
+    transition: background-color 0.2s ease;
+    background-color: ${props => (props.isDraggingOver ? "lightgreen" : "white")};
+    flex-grow: 1;
+    min-height: 100px;
+    `
+ 
+ const Column = props => {
+     
+     
+     return (
+        <Container>
+            <Title>{props.column.title}</Title>
+                {/* Droppable has one required prop: a droppable id that needs to be unique within the DragDropContext*/}
+            <Droppable droppableId={props.column.id}>
+            {/* Droppable expects its child to be a function that returns a react component*/}
+                {(provided, snapshot) => (
+                    // the first argument to this function is called "provided", which is an object that serves a few important purposes. The provider object has a property called "droppableProps": these are props that need to be applied to the component that you want to designate as your droppable 
+                    <TaskList
+                    // the "provided" object has a property called "innerRef", which is a property used to supply the DOM node of the component to react-beautiful-dnd
+                    ref={provided.innerRef} {...provided.droppableProps}
+                    isDraggingOver={snapshot.isDraggingOver}
+                    >
+                        {props.tasks.map((task, index) => (
+                            <Task key={task.id} task={task} index={index}/> 
+                            ))}
+                        {/* a placeholder is a react element used to increase the available space in a droppable during a drag when it's needed: the placeholder needs to be added as a child of the component that you designate as the droppable */}
+                        {provided.placeholder}
+                    </TaskList>
+                )}
+            </Droppable>
+        </Container>
+     )
+    }
+    
+    export default Column;
+    
+    
+    // class Column extends React.Component {
+       
+    //     render() { 
+    //         return this.props.column.title ;
+    //     }
+    // }
+     
+    // export default Column;
+ 
---
+++ drag_and_drop_stuff/react-beautiful-dnd (copy)/src/Task.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
+
+const Container = styled.div`
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+    padding: 8px;
+    margin-bottom: 8px;
+    color: white;
+    display: flex;
+    background-color: ${props => (props.isDragging ? "grey" : "purple")};`
+    // conditionally styling the background
+
+// const Handle = styled.div`
+//     width: 20px;
+//     height: 20px;
+//     background-color: orange;
+//     border-radius: 4px;
+//     margin-right: 8px;
+//     :hover {cursor: pointer};`
+
+const Task = props => {
+    return (
+        <Draggable draggableId={props.task.id} index={props.index}>
+            {/* Draggable also expects its child to be a function */}
+            {(provided, snapshot) => (
+                <Container
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                ref={provided.innerRef}
+                isDragging={snapshot.isDragging}
+                // here, we are passing a prop called isDragging to my style component which has the value of the snapshot.isDragging property; using the styled-components API, I can conditionally set the background-colour of the component based on this props
+                >
+                {/* <Handle {...provided.dragHandleProps} */}
+
+                 {props.task.content}
+            </Container>)}
+        </Draggable>
+    )
+}
+export default Task
