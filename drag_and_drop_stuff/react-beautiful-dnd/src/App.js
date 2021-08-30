c78c1bd32cbdba5c9f21e114e39be7255489e7a9
rdionis-elshoff <dionisio@elsweyer-hoffmann.de>
2021-08-30 16:45:21 +0200

    adding drag and drop features

--- drag_and_drop_stuff/react-beautiful-dnd/src/App.js
+++ drag_and_drop_stuff/react-beautiful-dnd/src/App.js
@@ -1,9 +1,13 @@
 import React, {useState} from "react"
 import "@atlaskit/css-reset"
+import styled from 'styled-components'
 import { DragDropContext } from 'react-beautiful-dnd'
 import initialData from "./initial-data"
 import Column from "./Column"
 
+const Container = styled.div`
+    display: flex;
+`
 
 const App = () => {
    
@@ -17,31 +21,60 @@ const App = () => {
         return
         }
 
-        if (
-            destination.droppableId === source.droppableId &&
-            destination.index === source.index
-        ) {
-            return
-        }
-        const column = state.columns[source.droppableId]
-        const newTaskIds = Array.from(column.taskIds)
-        newTaskIds.splice(source.index, 1) // this means: from the index "source.index", we want to remove 1 item
-        newTaskIds.splice(destination.index, 0, draggableId) // this means: from the index "destination.index" I am going to remove nothing (0) and I am going to add the draggableId
+        // if (
+        //     destination.droppableId === source.droppableId && // when we are dragging and dropping stuff inside the same element
+        //     destination.index === source.index
+        // ) {
+        //     return
+        // }
+        // const column = state.columns[source.droppableId]
+        const start = state.columns[source.droppableId]
+        const finish = state.columns[destination.droppableId]
+
+        if (start === finish) {
+
+            const newTaskIds = Array.from(start.taskIds)
+            newTaskIds.splice(source.index, 1) // this means: from the index "source.index", we want to remove 1 item
+            newTaskIds.splice(destination.index, 0, draggableId) // this means: from the index "destination.index" I am going to remove nothing (0) and I am going to add the draggableId
         
-        const newColumn = {
-            ...column,
-            taskIds: newTaskIds,
+            const newColumn = {
+                ...start,
+                taskIds: newTaskIds,
+            }
+
+            setState({          
+                ...state,
+                columns:{
+                  ...state.columns,
+                [newColumn.id]: newColumn, 
+                }
+            })}
+        // Moving from one list to another
+        const startTaskIds = Array.from(start.taskIds)
+        startTaskIds.splice(source.index, 1)
+        const newStart = {
+            ...start,
+            taskIds: startTaskIds,
         }
 
-        setState({          
+        const finishTaskIds = Array.from(finish.taskIds)
+        finishTaskIds.splice(destination.index, 0, draggableId)
+        const newFinish = {
+            ...finish,
+            taskIds: finishTaskIds,
+        }
+
+        setState({
             ...state,
-            columns:{
-              ...state.columns,
-              [newColumn.id]: newColumn, 
+            columns: {
+                ...state.columns,
+                [newStart.id]: newStart, 
+                [newFinish.id]: newFinish,
             }
+
         })
-     
       }
+
     
 
     return (
@@ -51,15 +84,18 @@ const App = () => {
         onDragEnd={onDragEnd}
         // called at the end of a drag (this is the ONLY REQUIRED CALLBACK for the DragDropContext)
     >
+        <Container>
+
 
     {state.columnOrder.map(columnId => {
         const column = state.columns[columnId]
         const tasks = column.taskIds.map(taskId => state.tasks[taskId])
         
         return (
-        <Column key={column.id} column={column} tasks={tasks}/>
-        )
-    })}
+            <Column key={column.id} column={column} tasks={tasks}/>
+            )
+        })}
+        </Container>
     </DragDropContext>
     )
 }
--- drag_and_drop_stuff/react-beautiful-dnd/src/Task.jsx
+++ drag_and_drop_stuff/react-beautiful-dnd/src/Task.jsx
@@ -7,20 +7,33 @@ const Container = styled.div`
     border-radius: 2px;
     padding: 8px;
     margin-bottom: 8px;
-    background-color: purple;
-    color: white`
+    color: white;
+    display: flex;
+    background-color: ${props => (props.isDragging ? "grey" : "purple")};`
+    // conditionally styling the background
 
-      
+// const Handle = styled.div`
+//     width: 20px;
+//     height: 20px;
+//     background-color: orange;
+//     border-radius: 4px;
+//     margin-right: 8px;
+//     :hover {cursor: pointer};`
+
 const Task = props => {
     return (
         <Draggable draggableId={props.task.id} index={props.index}>
             {/* Draggable also expects its child to be a function */}
-            {provided => (
-            <Container
+            {(provided, snapshot) => (
+                <Container
                 {...provided.draggableProps}
                 {...provided.dragHandleProps}
                 ref={provided.innerRef}
-            >
+                isDragging={snapshot.isDragging}
+                // here, we are passing a prop called isDragging to my style component which has the value of the snapshot.isDragging property; using the styled-components API, I can conditionally set the background-colour of the component based on this props
+                >
+                {/* <Handle {...provided.dragHandleProps} */}
+
                  {props.task.content}
             </Container>)}
         </Draggable>
