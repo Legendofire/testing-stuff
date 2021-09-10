6e360a68fe43ddb96a3223440d3fdc079dcc8f9f
rdionis-elshoff <dionisio@elsweyer-hoffmann.de>
2021-09-10 11:44:18 +0200

    trying new draggable libraries

---
+++ react-draggable-tutorial/README.md
@@ -0,0 +1,3 @@
+# Create React Draggable Component with react-draggable Package
+
+Check out the tutorial [here](https://www.positronx.io/create-react-draggable-component-with-react-draggable-package/).
--- react-draggable-tutorial/src/index.js
+++ react-draggable-tutorial/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Board from './components/Board';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-      <Board knightPosition = {[7, 4]} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
