0ca5e620dfc54b275973b8b0348b91d4043fc2a7
rdionis-elshoff <dionisio@elsweyer-hoffmann.de>
2021-09-13 15:45:36 +0200

    multi drag and drop

--- drag_and_drop/multi-list-drag-and-drop/src/App.js
+++ drag_and_drop/multi-list-drag-and-drop/src/App.js
@@ -1,11 +1,95 @@
-import React from 'react'
+import { useReducer } from 'react';
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import tw from "xwind";
+
+
+
+
+const dragReducer = (state, action) => {
+  return state;
+};
+
 
 const App = () => {
+
+  const data = [
+    {
+      id: "5f832341cc119a50d1adb972",
+      picture: "http://placehold.it/32x32",
+      name: {
+        first: "Goff",
+        last: "Robbins",
+      },
+    },
+  ];
+
+  const [state, dispatch] = useReducer(dragReducer, {
+    items: data,
+  });
+
+  const onDragEnd = useCallback((result) => {
+    if (result.reason === "DROP") {
+      if (!result.destination) {
+        return;
+      }
+      dispatch({
+        type: "MOVE",
+        from: result.source.droppableId,
+        to: result.destination.droppableId,
+        fromIndex: result.source.index,
+        toIndex: result.destination.index,
+      });
+    }
+  }, []);
+
   return (
-    <div>
-      <h1>Multi Drag and Drop</h1>
+
+    <DragDropContext onDragEnd={onDragEnd}>
+           
+    <div className='flex flex-row h-screen p-4'>
+     <Droppable droppableId="items" type="PERSON">
+        {(provided, snapshot) => {
+          return (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className=
+                {snapshot.isDraggingOver}
+            >
+                      {state.items?.map((person, index) => {
+          return (
+            <Draggable key={person.id} draggableId={person.id} index={index}>
+              {(provided, snapshot) => {
+                return (
+                  <div
+                    className={snapshot.isDragging}
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <div>
+                      <img
+                        src={person.picture}
+                      />
+                      <span>
+                        {person.name.first} {person.name.last}
+                      </span>
+                    </div>
+                  </div>
+                );
+              }}
+            </Draggable>
+          );
+        })}
+
+              {provided.placeholder}
+            </div>
+          );
+        }}
+      </Droppable>
     </div>
+</DragDropContext>
+
   )
 }
-
 export default App
